11f764db12e574415cdd3cf6d8c70e3722666f14
Mateus Luiz <mateuslfreitasb@gmail.com>
2019-10-04 11:27:22 -0300

    Wrap the functions and make the repo upload asynchronously

--- lib/crawley.ex
+++ lib/crawley.ex
@@ -1,2 +1,24 @@
 defmodule Crawley do
+  def run(%{lang: lang, per_page: per_page, page: page}) do
+    repos = Crawley.RepoUtils.get_repositories(%{
+      lang: lang,
+      per_page: per_page,
+      page: page
+    })
+    
+    download_repos(t)
+  end
+
+  defp download_repos(repos), do: Enum.map(repos, &git_clone_async/1)
+
+  defp git_clone_async(repo) do
+    repo_id = repo["id"]
+    repo_clone_url = repo["clone_url"]
+
+    spawn fn ->
+      Crawley.RepoUtils.clone_repo(repo_id, repo_clone_url)
+      Crawley.S3.upload_repo("crawley-repos", repo_id)
+      Crawley.RepoUtils.delete_repo(repo_id)
+    end
+  end
 end
