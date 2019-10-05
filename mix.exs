bcf8264737793c63389c23525c0df7c49c2478c4
rodriggochaves <rodriggo.chaves@gmail.com>
2019-10-05 16:21:52 -0300

    📦  config(cd): add ex-docs dependency

--- mix.exs
+++ mix.exs
@@ -29,7 +29,8 @@ defmodule Crawley.MixProject do
       {:hackney, "~> 1.9"},
       {:poison, "~> 3.0"},
       {:git_cli, "~> 0.3"},
-      {:exvcr, "~> 0.10", only: :test}
+      {:exvcr, "~> 0.10", only: :test},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
