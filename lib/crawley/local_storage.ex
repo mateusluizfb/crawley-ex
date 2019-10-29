c23a087d21b50b6af3154938e45e21323e68d690
rodriggochaves <rodriggo.chaves@gmail.com>
2019-10-28 21:11:32 -0300

    🔄  refactor(local storage): extract .zip_folder to Storage module

--- lib/crawley/local_storage.ex
+++ lib/crawley/local_storage.ex
@@ -3,16 +3,11 @@ defmodule Crawley.LocalStorage do
 
   @impl Crawley.Storage
   def upload_repo(remote_folder_name, repo_name) do
-    zip_folder(repo_name)
+    Crawley.Storage.zip_folder(repo_name)
 
     File.rename(
       "./tmp/#{repo_name}/#{repo_name}.zip",
       "./tmp/buckets/#{remote_folder_name}/#{repo_name}.zip"
     )
-  end
-
-  defp zip_folder(repo_name) do
-    files = File.ls!("./tmp/#{repo_name}") |> Enum.map(&String.to_charlist/1)
-    :zip.create("./tmp/#{repo_name}/#{repo_name}.zip", files)
   end
 end
--- lib/crawley/s3.ex
+++ lib/crawley/s3.ex
@@ -1,15 +1,10 @@
 defmodule Crawley.S3 do
   def upload_repo(bucket_name, repo_name) do
-    zip_folder(repo_name)
+    Crawley.Storage.zip_folder(repo_name)
 
     "./tmp/#{repo_name}/#{repo_name}.zip"
       |> ExAws.S3.Upload.stream_file
       |> ExAws.S3.upload(bucket_name, "#{repo_name}.zip")
       |> ExAws.request(region: "sa-east-1")
-  end
-
-  defp zip_folder(repo_name) do
-    files = File.ls!("./tmp/#{repo_name}") |> Enum.map(&String.to_charlist/1)
-    :zip.create("./tmp/#{repo_name}/#{repo_name}.zip", files)
   end
 end
