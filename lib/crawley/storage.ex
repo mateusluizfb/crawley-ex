ae18eabb60b984892ed77172f23e55ece08815b2
Mateus Luiz <mateuslfreitasb@gmail.com>
2019-11-05 19:40:47 -0300

    Fix files zipped

--- lib/crawley/storage.ex
+++ lib/crawley/storage.ex
@@ -2,7 +2,11 @@ defmodule Crawley.Storage do
   @callback upload_repo(String.t, String.t) ::  {:ok, term} | {:error, term}
 
   def zip_folder(repo_name) do
-    files = File.ls!("./tmp/#{repo_name}") |> Enum.map(&String.to_charlist/1)
+    path = "./tmp/#{repo_name}"
+    files = File.ls!(path)
+            |> Enum.map(fn filename -> Path.join(path, filename) end)
+            |> Enum.map(&String.to_charlist/1)
+
     :zip.create("./tmp/#{repo_name}/#{repo_name}.zip", files)
   end
 end
